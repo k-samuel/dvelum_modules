724c39654c8d22a74a6c83aef4c53fa000291a3c
Kirill <kirill.a.egorov@gmail.com>
2016-04-28 00:49:13 +0300

    Articles module config

---
+++ modules/DVelum/Articles/classes/Dvelum/Backend/Articles/Installer.php
@@ -0,0 +1,17 @@
+<?php
+class Dvelum_Backend_Articles_Installer extends Externals_Installer
+{
+    /**
+     * @var array
+     */
+    protected $errors = [];
+
+    /**
+     * Install
+     * @param Config_Abstract $applicationConfig
+     * @return boolean
+     */
+    public function run(Config_Abstract $applicationConfig){
+
+    }
+}
--- modules/DVelum/Articles/config.php
+++ modules/DVelum/Articles/config.php
@@ -6,7 +6,14 @@ return [
     'name' => 'DVelum Articles',
     'configs' => './configs',
     'locales' => './locales',
+    'resources' =>'./resources',
     'autoloader'=> [
         './classes'
-    ]
+    ],
+    'objects' =>[
+        'dvelum_article',
+        'dvelum_article_category',
+        'dvelum_article_related_category_to_dvelum_article_category'
+    ],
+    'post-install'=>'Dvelum_Backend_Articles_Installer'
 ];
