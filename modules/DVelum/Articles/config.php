2783dfc82511888d59bbaed23f3e6f83b2f8bd23
Kirill <kirill.a.egorov@gmail.com>
2016-10-17 13:18:34 +0300

    Articles fix. Backoffice ui. Articles pager

--- modules/DVelum/Articles/config.php
+++ modules/DVelum/Articles/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'id' => 'dvelum_articles',
-    'version' => '1.0.4',
+    'version' => '1.0.5',
     'author' => 'Kirill Egorov',
     'name' => 'DVelum Articles',
     'configs' => './configs',
